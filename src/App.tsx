f952f942cb13853632bc2d05623d220a8e7cc803
Ilya Khorobrykh <horobryh17@gmail.com>
2022-03-30 15:01:31 +0300

    finished 2nd lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,65 @@
 import React from 'react';
 import './App.css';
 
-
 function App() {
+    console.log('App is rendering');
     return (
         <div>
-            This is APP component
+            <AppTitle/>
             <Rating/>
             <Accordion/>
         </div>
     );
 }
 
+function AppTitle() {
+    console.log('AppTitle is rendering');
+    return <>This is APP component</>;
+}
+
 function Rating() {
+    console.log('Rating is rendering');
     return (
         <>
-          <Star/>
-          <Star/>
-          <Star/>
-          <Star/>
-          <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
         </>
-    )
+    );
 }
 
 function Accordion() {
+    console.log('Accordion is rendering');
     return (
-        <div>
-            <h3>Menu</h3>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
-        </div>
-    )
+        <>
+            <AccordionTitle/>
+            <AccordionBody/>
+        </>
+    );
+}
+
+function AccordionTitle() {
+    console.log('AccordionTitle is rendering');
+    return <h3>Menu</h3>;
+}
+
+function AccordionBody() {
+    console.log('AccordionBody is rendering');
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    );
 }
 
 function Star() {
-
-    return <div>Star</div>
-
+    console.log('Star is rendering');
+    return <div>Star</div>;
 }
+
 
 export default App;
